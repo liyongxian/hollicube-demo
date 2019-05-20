f09c9561113621b3100c06d25b784b13618d9d2c
lyx <lyx@LAPTOP-MR5JI2P1>
2019-05-20 18:52:34 +0800

    Testing

--- src/main/java/hollysys_demo/hollysys_demo/controller/SampleController.java
+++ src/main/java/hollysys_demo/hollysys_demo/controller/SampleController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SampleController {
 	@RequestMapping("/")
     String home() {
-        return "Hello World!";
+        return "Hello World! Hello 中国！";
     }
 }
 
