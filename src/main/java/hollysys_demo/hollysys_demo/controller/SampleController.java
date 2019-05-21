0472b431a326762020ebe406d1e0dfd6c1c71222
李永仙 <liyongxian@inspur.com>
2019-05-21 09:48:58 +0800

    Update SampleController.java

--- src/main/java/hollysys_demo/hollysys_demo/controller/SampleController.java
+++ src/main/java/hollysys_demo/hollysys_demo/controller/SampleController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SampleController {
 	@RequestMapping("/")
     String home() {
-        return "Hello China Beijing";
+        return "Hello China Beijing111";
     }
 }
 
